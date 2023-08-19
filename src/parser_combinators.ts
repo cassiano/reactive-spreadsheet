f9555c164e37cba960c340267edb7af3f20b4ecb
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2023-08-19 10:44:16 -0300

    Mega revisão/simplificação dos parsers

--- src/parser_combinators.ts
+++ src/parser_combinators.ts
@@ -23,88 +23,44 @@ export const or =
   <A, B>(parser1: Parser<A>, parser2: Parser<B>): Parser<A | B> =>
   input => {
     const [result1, rest1] = parser1(input)
-    if (!isError(result1)) return [result1, rest1]
 
-    const [result2, rest2] = parser2(input)
-    if (!isError(result2)) return [result2, rest2]
-
-    return [error('(or) neither parser 1 nor parser 2 satisfied'), input]
+    return !isError(result1) ? [result1, rest1] : parser2(input)
   }
 export const or2 = or
-export const or3 =
-  <A, B, C>(parser1: Parser<A>, parser2: Parser<B>, parser3: Parser<C>): Parser<A | B | C> =>
-  input => {
-    const [result1, rest1] = parser1(input)
-    if (!isError(result1)) return [result1, rest1]
 
-    const [result2, rest2] = parser2(input)
-    if (!isError(result2)) return [result2, rest2]
+export const or3 = <A, B, C>(parser1: Parser<A>, parser2: Parser<B>, parser3: Parser<C>): Parser<A | B | C> =>
+  or(parser1, or2(parser2, parser3))
 
-    const [result3, rest3] = parser3(input)
-    if (!isError(result3)) return [result3, rest3]
+export const or4 = <A, B, C, D>(
+  parser1: Parser<A>,
+  parser2: Parser<B>,
+  parser3: Parser<C>,
+  parser4: Parser<D>
+): Parser<A | B | C | D> => or(parser1, or3(parser2, parser3, parser4))
 
-    return [error('(or) none of parsers 1..3 satisfied'), input]
-  }
-export const or4 =
-  <A, B, C, D>(parser1: Parser<A>, parser2: Parser<B>, parser3: Parser<C>, parser4: Parser<D>): Parser<A | B | C | D> =>
-  input => {
-    const [result1, rest1] = parser1(input)
-    if (!isError(result1)) return [result1, rest1]
-
-    const [result2, rest2] = parser2(input)
-    if (!isError(result2)) return [result2, rest2]
-
-    const [result3, rest3] = parser3(input)
-    if (!isError(result3)) return [result3, rest3]
-
-    const [result4, rest4] = parser4(input)
-    if (!isError(result4)) return [result4, rest4]
-
-    return [error('(or) none of parsers 1..4 satisfied'), input]
-  }
-export const or5 =
-  <A, B, C, D, E>(
-    parser1: Parser<A>,
-    parser2: Parser<B>,
-    parser3: Parser<C>,
-    parser4: Parser<D>,
-    parser5: Parser<E>
-  ): Parser<A | B | C | D | E> =>
-  input => {
-    const [result1, rest1] = parser1(input)
-    if (!isError(result1)) return [result1, rest1]
-
-    const [result2, rest2] = parser2(input)
-    if (!isError(result2)) return [result2, rest2]
-
-    const [result3, rest3] = parser3(input)
-    if (!isError(result3)) return [result3, rest3]
-
-    const [result4, rest4] = parser4(input)
-    if (!isError(result4)) return [result4, rest4]
-
-    const [result5, rest5] = parser5(input)
-    if (!isError(result5)) return [result5, rest5]
-
-    return [error('(or) none of parsers 1..5 satisfied'), input]
-  }
+export const or5 = <A, B, C, D, E>(
+  parser1: Parser<A>,
+  parser2: Parser<B>,
+  parser3: Parser<C>,
+  parser4: Parser<D>,
+  parser5: Parser<E>
+): Parser<A | B | C | D | E> => or(parser1, or4(parser2, parser3, parser4, parser5))
 
 export const and = <A, B>(parser1: Parser<A>, parser2: Parser<B>): Parser<[A, B]> =>
   sequence(parser1, result1 => map(parser2, result2 => [result1, result2]))
 export const and2 = and
+
 export const and3 = <A, B, C>(parser1: Parser<A>, parser2: Parser<B>, parser3: Parser<C>): Parser<[A, B, C]> =>
-  sequence(parser1, result1 => sequence(parser2, result2 => map(parser3, result3 => [result1, result2, result3])))
+  map(and(parser1, and2(parser2, parser3)), ([result1, results]) => [result1, ...results])
+
 export const and4 = <A, B, C, D>(
   parser1: Parser<A>,
   parser2: Parser<B>,
   parser3: Parser<C>,
   parser4: Parser<D>
 ): Parser<[A, B, C, D]> =>
-  sequence(parser1, result1 =>
-    sequence(parser2, result2 =>
-      sequence(parser3, result3 => map(parser4, result4 => [result1, result2, result3, result4]))
-    )
-  )
+  map(and(parser1, and3(parser2, parser3, parser4)), ([result1, results]) => [result1, ...results])
+
 export const and5 = <A, B, C, D, E>(
   parser1: Parser<A>,
   parser2: Parser<B>,
@@ -112,83 +68,7 @@ export const and5 = <A, B, C, D, E>(
   parser4: Parser<D>,
   parser5: Parser<E>
 ): Parser<[A, B, C, D, E]> =>
-  sequence(parser1, result1 =>
-    sequence(parser2, result2 =>
-      sequence(parser3, result3 =>
-        sequence(parser4, result4 => map(parser5, result5 => [result1, result2, result3, result4, result5]))
-      )
-    )
-  )
-
-// export const and =
-//   <A, B>(parser1: Parser<A>, parser2: Parser<B>): Parser<[A, B]> =>
-//   input => {
-//     const [result1, rest1] = parser1(input)
-//     if (isError(result1)) return [result1, input]
-
-//     const [result2, rest2] = parser2(rest1)
-//     if (isError(result2)) return [result2, input]
-
-//     return [[result1, result2], rest2]
-//   }
-// export const and2 = and
-// export const and3 =
-//   <A, B, C>(parser1: Parser<A>, parser2: Parser<B>, parser3: Parser<C>): Parser<[A, B, C]> =>
-//   input => {
-//     const [result1, rest1] = parser1(input)
-//     if (isError(result1)) return [result1, input]
-
-//     const [result2, rest2] = parser2(rest1)
-//     if (isError(result2)) return [result2, input]
-
-//     const [result3, rest3] = parser3(rest2)
-//     if (isError(result3)) return [result3, input]
-
-//     return [[result1, result2, result3], rest3]
-//   }
-// export const and4 =
-//   <A, B, C, D>(parser1: Parser<A>, parser2: Parser<B>, parser3: Parser<C>, parser4: Parser<D>): Parser<[A, B, C, D]> =>
-//   input => {
-//     const [result1, rest1] = parser1(input)
-//     if (isError(result1)) return [result1, input]
-
-//     const [result2, rest2] = parser2(rest1)
-//     if (isError(result2)) return [result2, input]
-
-//     const [result3, rest3] = parser3(rest2)
-//     if (isError(result3)) return [result3, input]
-
-//     const [result4, rest4] = parser4(rest3)
-//     if (isError(result4)) return [result4, input]
-
-//     return [[result1, result2, result3, result4], rest4]
-//   }
-// export const and5 =
-//   <A, B, C, D, E>(
-//     parser1: Parser<A>,
-//     parser2: Parser<B>,
-//     parser3: Parser<C>,
-//     parser4: Parser<D>,
-//     parser5: Parser<E>
-//   ): Parser<[A, B, C, D, E]> =>
-//   input => {
-//     const [result1, rest1] = parser1(input)
-//     if (isError(result1)) return [result1, input]
-
-//     const [result2, rest2] = parser2(rest1)
-//     if (isError(result2)) return [result2, input]
-
-//     const [result3, rest3] = parser3(rest2)
-//     if (isError(result3)) return [result3, input]
-
-//     const [result4, rest4] = parser4(rest3)
-//     if (isError(result4)) return [result4, input]
-
-//     const [result5, rest5] = parser5(rest4)
-//     if (isError(result5)) return [result5, input]
-
-//     return [[result1, result2, result3, result4, result5], rest5]
-//   }
+  map(and(parser1, and4(parser2, parser3, parser4, parser5)), ([result1, results]) => [result1, ...results])
 
 export const map =
   <A, B>(parser: Parser<A>, fn: (value: A) => B): Parser<B> =>
@@ -215,14 +95,23 @@ export const many2 = <A>(parser: Parser<A>): Parser<A[]> => manyN(parser, 2)
 
 export const concat = (parser: Parser<string[]>): Parser<string> => map(parser, chars => chars.join(''))
 
+export const precededBy = <A, B>(parser1: Parser<A>, parser2: Parser<B>): Parser<B> =>
+  map(and(parser1, parser2), ([_, result2]) => result2)
+
+export const succeededBy = <A, B>(parser1: Parser<A>, parser2: Parser<B>): Parser<A> =>
+  map(and(parser1, parser2), ([result1, _]) => result1)
+
 export const letter = satisfy(char => /[a-z]/i.test(char))
+
 export const digit = map(
   satisfy(char => /\d/.test(char)),
   digit => +digit
 )
+
 export const character = (singleChar: SingleChar) => satisfy(char => char === singleChar)
 
 export const letters = many1(letter)
+
 export const digits = many1(digit)
 
 export const identifierChar = map(or3(letter, digit, character('_')), res => res.toString())
@@ -237,8 +126,11 @@ export const OPERATIONS: { [name: string]: SingleChar } = {
 }
 
 export const empty: Parser<string> = input => ['', input]
+
 export const optional = <T>(parser: Parser<T>) => or(parser, empty)
+
 export const ref = map(and(letters, digits), result => result.flat(2).join(''))
+
 export const operator = or5(
   character(OPERATIONS.addition),
   character(OPERATIONS.subtraction),
@@ -246,13 +138,17 @@ export const operator = or5(
   character(OPERATIONS.division),
   character(OPERATIONS.exponentiation)
 )
+
 export const plus = character('+')
 export const minus = character('-')
 export const sign = or(plus, minus)
-export const natural = map(and(optional(plus), digits), ([_, digs]) =>
+
+export const natural = map(precededBy(optional(plus), digits), digs =>
   digs.reduce((acc, dig, i) => acc + dig * 10 ** (digs.length - (i + 1)), 0)
 )
+
 export const integer = map(and(optional(sign), natural), ([signChar, nat]) => (signChar === '-' ? -1 : +1) * nat)
+
 export const naturalGreaterThanZero: Parser<number> = input => {
   const [result, rest] = natural(input)
 
@@ -261,17 +157,20 @@ export const naturalGreaterThanZero: Parser<number> = input => {
 
   return [result, rest]
 }
+
 export const period = character('.')
+
 export const float = map(
-  and3(integer, period, natural),
-  ([int, _, nat]) => int + (Math.sign(int) * nat) / 10 ** Math.trunc(Math.log10(nat) + 1)
+  and(integer, precededBy(period, natural)),
+  ([int, nat]) => int + (nat === 0 ? 0 : (Math.sign(int) * nat) / 10 ** Math.trunc(Math.log10(nat) + 1))
 )
+
 export const numeric = or(float, integer)
+
 export const operand = or(ref, numeric)
+
 export const exp = and(operand, many(and(operator, operand)))
-export const precededBy = <A, B>(parser1: Parser<A>, parser2: Parser<B>): Parser<B> =>
-  map(and(parser1, parser2), ([_, result2]) => result2)
-export const succeededBy = <A, B>(parser1: Parser<A>, parser2: Parser<B>): Parser<A> =>
-  map(and(parser1, parser2), ([result1, _]) => result1)
+
 export const equals = character('=')
+
 export const formula = precededBy(equals, exp)
